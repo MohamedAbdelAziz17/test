63bfa6ae046ad092e6971179eaff066ae30557ee
MohamedAbdelAziz17 <79328998+MohamedAbdelAziz17@users.noreply.github.com>
2021-02-21 12:03:33 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Math.h"
+#define MAX 10
+int main()
+{
+<<<<<<< HEAD
+int num1,num2,sum;
+sum = num1+num2;
+    printf("MAX %d",MAX);
+printf("Mohamed");
+printf("\n %d",sum);
+=======
+
+printf("MAX %d",MAX);
+printf("Mohamed");
+printf("Moaz");
+printf("Nada");
+
+
+>>>>>>> Developer
+    return 0;
+}
